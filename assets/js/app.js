c76c27b98ed2db66aca37d0f397c0a8bfad19d47
James Howe <jhowe1790@outlook.com>
2018-04-30 13:38:17 +0100

    feat(*): add modal function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 
-});
+  // Model
+  var close = $('#js-modal-close'),
+    modal = $('.modal'),
+    trigger = $('#js-modal-trigger');
+
+  trigger.click(function() {
+    modal.addClass('modal--active');
+    $('body').addClass('overlay-active');
+  })
+
+  close.click(function() {
+    $('body').removeClass('overlay-active');
+    modal.removeClass('modal--active');
+  });
+
 
