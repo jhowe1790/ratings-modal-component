39783e42aa8372a210b04f38d287fec410c59ee8
James Howe <jhowe1790@outlook.com>
2018-04-30 13:38:36 +0100

    feat(*): add form processing function

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,4 +15,66 @@ $(document).ready(function() {
     modal.removeClass('modal--active');
   });
 
+  $('form').submit(function(event) {
 
+    $('.form-field').removeClass('form-field--error');
+    $('.form-field__hint').remove();
+
+    // Form data
+    var formData = {
+      'foodRating': $('input[name=foodRating]:checked').val(),
+      'deliveryRating': $('input[name=deliveryRating]:checked').val(),
+      'overallRating': $('input[name=overallRating]:checked').val()
+    };
+
+    // Process form
+    $.ajax({
+      type: 'POST',
+      url: 'process.php',
+      data: formData,
+      dataType: 'json',
+      encode: true
+    })
+
+    .done(function(data) {
+
+      console.log(data);
+
+      if (!data.success) {
+
+        // Error validation
+        if (data.errors.foodRating) {
+          $('#food-rating').addClass('form-field--error');
+          $('#food-rating').append('<div class="form-field__hint">' + data.errors.foodRating + '</div>');
+        }
+
+        if (data.errors.deliveryRating) {
+          $('#delivery-rating').addClass('form-field--error');
+          $('#delivery-rating').append('<div class="form-field__hint">' + data.errors.deliveryRating + '</div>');
+        }
+
+        if (data.errors.overallRating) {
+          $('#overall-rating').addClass('form-field--error');
+          $('#overall-rating').append('<div class="form-field__hint">' + data.errors.overallRating + '</div>');
+        }
+
+      } else {
+
+        var submission = '<div class="form__notification"> \
+							<h2 class="form__heading">' + data.message + '</h2> \
+              <p class="form__intro">We appreciate you taking the time to fill out the form. Here are the ratings you submitted:</p> \
+							<h3 class="form-field__heading">' + data.foodRating + '</h3>\
+							<h3 class="form-field__heading">' + data.deliveryRating + '</h3>\
+							<h3 class="form-field__heading">' + data.overallRating + '</h3>';
+
+        $('#js-modal-trigger').text('Review submitted');
+        $('#js-modal-trigger').addClass('button--disabled');
+        $('.button--disabled').attr('disabled', true);
+        $('.form').remove();
+        $('.modal__inner').append(submission);
+      }
+    })
+
+    event.preventDefault();
+  });
+});
